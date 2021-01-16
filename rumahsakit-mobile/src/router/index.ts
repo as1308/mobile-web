6c73ee9297fecc6cd0f4c68d354b4e47067e7398
Ubuntu <ubuntu@ip-172-31-11-38.ec2.internal>
2021-01-16 05:31:26 +0000

    mobile

--- rumahsakit-mobile/src/router/index.ts
+++ rumahsakit-mobile/src/router/index.ts
@@ -5,7 +5,7 @@ import Home from '../views/Home.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/home'
   },
   {
     path: '/home',
@@ -15,22 +15,6 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/message/:id',
     component: () => import('../views/ViewMessage.vue')
-  },
-  {
-    path:'/login',
-    name:'login',
-    component:() => import('../views/login.vue')
-  },
-  {
-    path:'/kelas',
-    name:'kelas',
-    component:() => import('../views/kelas.vue')
-  }
-  ,
-  {
-    path:'/kelas/:id',
-    name:'kelasdetail',
-    component:() => import('../views/kelasdetail.vue')
   }
 ]
 
---
+++ src/views/kelas.vue
@@ -0,0 +1,79 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="success">
+                <ion-title>Kelas</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>
+                    <ion-label>
+                        kelas 1
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata Kuiah</p>
+                    </ion-label>
+                    </ion-item>
+                <ion-item>
+                    <ion-label>
+                        kelas 2
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata Kuiah</p>
+                    </ion-label>
+                    </ion-item>
+                <ion-item>
+                    <ion-label>
+                        kelas 3
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata Kuiah</p>
+                    </ion-label>
+                    </ion-item>
+                <ion-item>
+                    <ion-label>
+                        kelas 4
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata Kuiah</p>
+                    </ion-label>
+                    </ion-item>
+                <ion-item>
+                    <ion-label>
+                        kelas 5
+                        <span class="rata-kanan">No Ruang</span>
+                        <p>Mata Kuiah</p>
+                    </ion-label>
+                    </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {
+    IonPage,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel
+} from'@ionic/vue'
+import{defineComponent} from 'vue'
+
+export default defineComponent({
+    components:{
+    IonPage,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel
+    }
+})
+</script>
+
+<style scoped>
+.rata-kanan{
+    float:right;
+}
+</style>
